e4b3c401f03922b3057d216e1eb40184aee923cc
Wang Quanquan (EXT Reply) <q.wang@reply.com>
2023-02-22 22:03:31 +0100

    Error modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
-import ErrorModal from "./components/UI/ErrorModal";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
@@ -14,7 +13,6 @@ function App() {
   };
   return (
     <div>
-      <ErrorModal title="An error occured" message="Something went wrong!" />
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
     </div>
